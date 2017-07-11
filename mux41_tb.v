8f3cab2f6ea826d44716e4982c28f738a80d1e8c
Matthew Otto <saturnstethys@gmail.com>
2017-07-11 17:34:57 -0400

    same as mux21_tb.v, vill change to meet requirements soon

--- mux41_tb.v
+++ mux41_tb.v
@@ -1,28 +1,27 @@
-module mux41_tb;
-reg wD0, wD1, wD2, wD3, wS;	  // machine inputs
-wire wY						  // machine output
-reg [5:0] testvectors[0:100]; // [5:0] needs to be changed to [X:0] as X= truthTableLength-1
-reg [7:0] errors;			  // counts how many rows were incorrect
-reg rightY;					  // truth table expected output
+module mux21_tb;
+reg wD0, wD1, wS; 				 // machine inputs
+wire wY;						 // machine output
+reg [3:0] testvectors[0:50];    // [3:0] needs to be changed to [X:0] as X= truthTableLength-1 (DONE)
+reg [7:0] errors;			     // counts how many rows were incorrect
+reg [7:0] vectornum;		     // loop counterrows were incorrect
+reg rightY;		         // truth table expected output
 
 // Connect UUT to test bench signals
-mux41 #(1) utt( // the 1 gets passed as the first parameter, the bit width
+mux21 #(1) utt( // the 1 gets passed as the first parameter, the bit width
+.S	(wS),
 .D0 (wD0),
 .D1 (wD1),
-.D2 (wD2),
-.D3 (wD3),
-.S	(wS),
 .Y	(wY)
 );
 
-//Obviously not working from HERE
+// Remember to take a dump
 initial  begin
-    $dumpfile ("lab6_sm1.vcd"); 
+    $dumpfile ("lab7_muxes.vcd"); 
 	$dumpvars; 
 end 
 
 initial begin
-	$readmemb("testvec_lab6_sm1.txt", testvectors); 
+	$readmemb("testvec_lab7_mux21.txt", testvectors); //testvec is in binary
 	vectornum = 0;
 	errors = 0;
 end
@@ -32,17 +31,14 @@ always begin
 		$display("%1d tests completed with %1d errors.", vectornum, errors);
 		$finish;
 	end
-	
+	{wS, wD0, wD1, rightY} = testvectors[vectornum];
+	#1  // must allow state machine time to run its behavioral code
+	if (rightY !== wY) begin 
+		errors = errors+1;	// found incorrect output
+		$display("Select:%b D0:%b D1:%b incorrectly outputs Y=%b ", wS, wD0, wD1, wY );
+	end
+	vectornum = vectornum+1; // increments loop counter
+end
 endmodule
 
-//TO HERE
 
-
-
-
-
-
-
-
-
-	
